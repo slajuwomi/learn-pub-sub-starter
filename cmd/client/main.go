a096eca064412456176004c2505a9433920912b3
SLajuwomi <stephenlajuwomi@gmail.com>
2025-11-15 16:50:08 -0600

    learned about exchanges, queues and alot of other stuff

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	conString := "amqp://guest:guest@localhost:5672/"
+	con, err := amqp.Dial(conString)
+	if err != nil {
+		fmt.Println("Client dial failed: ", err)
+	}
+	defer con.Close()
+	fmt.Println("Connection successful")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Println("Failed to get username: ", err)
+	}
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	done := make(chan bool, 1)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	<-done
+	fmt.Println("Program is shutting down...")
+
 }
